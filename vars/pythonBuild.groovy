61fd3ed23ac194d74e23c997fdb79438c86c75cf
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-06-02 16:27:38 +0530

    Update pythonBuild.groovy

--- vars/pythonBuild.groovy
+++ vars/pythonBuild.groovy
@@ -1,7 +1,8 @@
 
 def call(){
-  sh 'python3 -m venv abhi-venv'
-  sh '. abhi-venv/bin/activate'
-  sh 'cd abhi-venv'
-  sh 'pip install -r requirements.txt'
+  // sh 'python3 -m venv abhi-venv'
+  // sh '. abhi-venv/bin/activate'
+  // sh 'cd abhi-venv'
+  // sh 'pip install -r requirements.txt'
+  sh 'echo "Hello world"'
 }
